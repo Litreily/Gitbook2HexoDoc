51c63b90fc8aae8ab5c27162a3449e4add0923cb
litreily <707922098wgt@gmail.com>
2019-01-26 15:28:32 +0800

    Add feature: arguments parsing

--- Gitbook2HexoDoc.py
+++ Gitbook2HexoDoc.py
@@ -1,8 +1,9 @@
 # coding: utf-8
-"""Convert gitbook documents to hexo blogs."""
 
 import os
 import re
+import sys
+import getopt
 
 import time
 import datetime
@@ -16,6 +17,8 @@ POST_DIR = r'E:/workspace/blog/source/_posts'    # hexo 博客文档目录
 
 
 class Gitbook2Hexo(object):
+    """Convert gitbook documents to hexo blogs."""
+
     SUMMARY = 'SUMMARY.md'
     TAB_INDENT = 2  # SUMMARY.md 文件tab缩进
     MAX_LEVEL = 3   # gitbook 文档目录最大深度
@@ -66,13 +69,15 @@ class Gitbook2Hexo(object):
         """Copy all resources from gitbook to blog."""
 
         try:
-            dupFrom = self.gitbook_path + self.res_dir
+            dupFrom = os.path.join(self.gitbook_path, self.res_dir)
             if os.path.exists(dupFrom):
                 dupTo = os.path.dirname(self.post_path) + '/' + self.res_dir
                 print('Duplicating {} from {} to {}...'.format(
                     self.res_dir, dupFrom, dupTo))
 
-                copytree(self.gitbook_path + self.res_dir, dupTo)
+                copytree(dupFrom, dupTo)
+            else:
+                print('<WARNING> Not found resources: ' + self.res_dir)
 
         except FileExistsError as e:
             print('<WARNING> Duplicating {0} fail, {1} is existed.'.format(
@@ -83,11 +88,11 @@ class Gitbook2Hexo(object):
         """Convert gitbook document to hexo blog."""
 
         try:
-            with open(self.gitbook_path + self.SUMMARY, 'r') as f:
+            with open(os.path.join(self.gitbook_path, self.SUMMARY), 'r') as f:
                 data = f.read()
         except FileNotFoundError as e:
             print('[ERROR] Not found file {}'.format(self.SUMMARY))
-            os._exit(1)
+            sys.exit(1)
 
         pattern = re.compile(r'( *)\* *\[(.*?)\]\((.*)\)')
         fileTree = re.findall(pattern, data)
@@ -103,7 +108,7 @@ class Gitbook2Hexo(object):
             try:
                 group_name = group[0] if not level else ', '.join(group[:level])
                 group_name = '[{}]'.format(group_name)
-                filepath = self.gitbook_path + file
+                filepath = os.path.join(self.gitbook_path, file.lstrip('/\\'))
                 filetime = self._get_file_createtime(filepath)
 
                 newfilepath = r'{}/{}-{}.md'.format(self.post_path, 
@@ -125,10 +130,39 @@ class Gitbook2Hexo(object):
 
         self._duplicate_resources()
 
+
+def usage():
+    print(
+        'usage: {} [-h][-s src][-d dst][-r resource]\n'.format(os.path.basename(sys.argv[0])) +
+        '\t-h\t\tshow this help message and exit\n'
+        '\t-s --src\troot dir of gitbook documents\n'
+        '\t-d --dst\tposts path of hexo blogs\n'
+        '\t-r\t\tresources files dir of gitbook documents, default "assets"\n'
+    )
+    sys.exit(2)
+
+def parse_args(argv):
+    try:
+        opts, args = getopt.getopt(argv, 'hs:d:r:', ['src=', 'dst='])
+    except getopt.GetoptError as e:
+        usage()
+
+    global GITBOOK_DIR, POST_DIR, RESOURCES_DIR
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+        elif opt in ('-s', '--src'):
+            GITBOOK_DIR = arg
+        elif opt in ('-d', '--dst'):
+            POST_DIR = arg
+        elif opt == '-r':
+            RESOURCES_DIR = arg
+    
         
-def main():
+def main(argv):
+    parse_args(argv)
     coverter = Gitbook2Hexo(GITBOOK_DIR, POST_DIR, RESOURCES_DIR)
     coverter.convert()
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
